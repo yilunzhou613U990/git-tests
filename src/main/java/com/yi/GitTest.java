d7b7f8395530034c04ab886608c03506c34aa6d7
yilun <yilunzhou671@gmail.com>
2023-03-13 23:01:03 +0800

    hot-fix first commit

--- src/main/java/com/yi/GitTest.java
+++ src/main/java/com/yi/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello world!");
         System.out.println("Hello world!2");
         System.out.println("Hello world!3");
+        System.out.println("Hello world!4");
     }
 }
